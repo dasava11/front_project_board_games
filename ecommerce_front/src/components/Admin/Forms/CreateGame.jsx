818dde0aae09221eb125f86688873daa78897eec
Mek <99274729+mgs1987@users.noreply.github.com>
2023-06-25 03:39:01 -0400

    comprobacion de forms, select multiple, toastify

--- ecommerce_front/src/components/Admin/Forms/CreateGame.jsx
+++ ecommerce_front/src/components/Admin/Forms/CreateGame.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+//import { postGames } from "../../../Redux/actions_creators/index";
+import validations from "./validations";
 import { CreateCategories } from "./CreateCategories";
 import {
   getAuthors,
@@ -10,19 +12,26 @@ import {
   getThematics,
 } from "../../../Redux/actions_creators/index";
 import { useDispatch, useSelector } from "react-redux";
+import { Select } from "antd";
 import "./creategames.css";
+const { Option } = Select;
 
 export default function CreateGame() {
-  const [input, setInput] = useState({});
+  const {
+    allAuthors,
+    allCategories,
+    allEditorials,
+    allDesigners,
+    allMechanics,
+    allThematics,
+    allLanguages,
+  } = useSelector((state) => state);
 
-  const authors = useSelector((state) => state.allAuthors);
-  const categories = useSelector((state) => state.allCategories);
-  const editorials = useSelector((state) => state.allEditorials);
-  const designers = useSelector((state) => state.allDesigners);
-  const mechanics = useSelector((state) => state.allMechanics);
-  const thematics = useSelector((state) => state.allThematics);
-  const languages = useSelector((state) => state.allLanguages);
+  const [input, setInput] = useState({});
+  const [modal, setModal] = useState(false);
+  const [errors, setErrors] = useState({});
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getAuthors());
     dispatch(getCategories());
@@ -31,15 +40,21 @@ export default function CreateGame() {
     dispatch(getLanguages());
     dispatch(getMechanics());
     dispatch(getThematics());
-  }, []);
+  }, [dispatch]);
+
+  const handleModalCreate = () => {
+    setModal(modal === true ? false : true);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
+    setErrors(validations(input));
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    //dispatch(postGames(input));
     setInput({
       name: "",
       released: "",
@@ -47,9 +62,7 @@ export default function CreateGame() {
       age: "",
       players_min: "",
       players_max: "",
-      rating: [],
       stock: 0,
-      active: true,
       image: [],
       weight: "",
       playing_time: 0,
@@ -62,183 +75,289 @@ export default function CreateGame() {
     });
     e.target.reset();
   };
-  console.log(authors);
+
   return (
-    <div className="form-maincontainer">
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={input.name}
-          onChange={handleChange}
-        />
-        <input
-          type="date"
-          placeholder="Released"
-          name="released"
-          value={input.released}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Price"
-          name="price"
-          value={input.price}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Age"
-          name="age"
-          value={input.age}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Players Min"
-          name="players_min"
-          value={input.players_min}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Players Max"
-          name="players_max"
-          value={input.players_max}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Rating"
-          name="rating"
-          value={input.rating}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Stock"
-          name="stock"
-          value={input.stock}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Active"
-          name="active"
-          value={input.active}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Image"
-          name="image"
-          value={input.image}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Weight"
-          name="weight"
-          value={input.weight}
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          placeholder="Playing time"
-          name="playingtime"
-          value={input.playing_time}
-          onChange={handleChange}
-        />
-        <select onChange={handleChange} name="authors">
-          Authors
-          <option defaultValue>Select an author</option>
-          {authors &&
-            authors.map((aut) => {
-              return (
-                <option key={aut.author_name} value={aut.author_name}>
-                  {aut.author_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="categories">
-          Categories
-          <option defaultValue>Select category</option>
-          {categories &&
-            categories.map((cat) => {
-              return (
-                <option key={cat.category_name} value={cat.category_name}>
-                  {cat.category_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="designers">
-          Designers
-          <option defaultValue>Select designer</option>
-          {designers &&
-            designers.map((des) => {
-              return (
-                <option key={des.designer_name} value={des.designer_name}>
-                  {des.designer_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="editorial">
-          Editorial
-          <option defaultValue>Select editorial</option>
-          {editorials &&
-            editorials.map((edit) => {
-              return (
-                <option key={edit.editorial_name} value={edit.editorial_name}>
-                  {edit.editorial_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="mechanic">
-          Mechanics
-          <option defaultValue>Select mechanic</option>
-          {mechanics &&
-            mechanics.map((mec) => {
-              return (
-                <option key={mec.mechanic_name} value={mec.mechanic_name}>
-                  {mec.mechanic_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="thematic">
-          Thematics
-          <option defaultValue>Select thematic</option>
-          {thematics &&
-            thematics.map((them) => {
-              return (
-                <option key={them.thematic_name} value={them.thematic_name}>
-                  {them.thematic_name}
-                </option>
-              );
-            })}
-        </select>
-        <select onChange={handleChange} name="thematic">
-          Lenguages
-          <option defaultValue>Select lenguage</option>
-          {languages &&
-            languages.map((len) => {
-              return (
-                <option key={len.language_name} value={len.language_name}>
-                  {len.language_name}
-                </option>
-              );
-            })}
-        </select>
-        <input type="submit" />
-      </form>
-      <div className="create-categories-container">
-        <CreateCategories />
+    <>
+      <div className="maincontainer">
+        <h2 className="title-form">Create Product</h2>
       </div>
-    </div>
+
+      <div className="form-container">
+        <form onSubmit={(e) => handleSubmit(e)} className="form">
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Game name"
+            name="name"
+            value={input.name}
+            onChange={handleChange}
+          />
+          {errors.name && <span>{errors.name}</span>}
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Released"
+            name="released"
+            value={input.released}
+            onChange={handleChange}
+          />
+          {errors.released && <span>{errors.released}</span>}
+          <input
+            className="inputs"
+            type="number"
+            placeholder="Price"
+            name="price"
+            value={input.price}
+            onChange={handleChange}
+            step=".01"
+            min={0}
+          />
+          {errors.price && <span>{errors.price}</span>}
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Age"
+            name="age"
+            value={input.age}
+            onChange={handleChange}
+            min={0}
+          />
+          {errors.age && <span>{errors.age}</span>}
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Players Min"
+            name="players_min"
+            value={input.players_min}
+            onChange={handleChange}
+            min={0}
+          />
+          {errors.players_min && <span>{errors.players_min}</span>}
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Players Max"
+            name="players_max"
+            value={input.players_max}
+            onChange={handleChange}
+            min={0}
+          />
+          {errors.players_max && <span>{errors.players_max}</span>}
+          <input
+            className="inputs"
+            type="number"
+            placeholder="Stock"
+            name="stock"
+            value={input.stock}
+            onChange={handleChange}
+            min={0}
+          />
+          {errors.stock && <span>{errors.stock}</span>}
+          <input
+            className="inputs"
+            type="text"
+            placeholder="Image"
+            name="image"
+            value={input.image}
+            onChange={handleChange}
+          />
+
+          <input
+            className="inputs"
+            type="number"
+            placeholder="Weight"
+            name="weight"
+            value={input.weight}
+            onChange={handleChange}
+            step=".01"
+            min={0}
+          />
+          {errors.weight && <span>{errors.weight}</span>}
+          <input
+            className="inputs"
+            type="number"
+            placeholder="Playing time"
+            name="playingtime"
+            value={input.playing_time}
+            onChange={handleChange}
+            min={0}
+          />
+          {errors.playing_time && <span>{errors.playing_time}</span>}
+          <Select
+            mode="multiple"
+            onChange={handleChange}
+            name="authors"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+            placeholder="Select authors"
+          >
+            {allAuthors &&
+              allAuthors.map((aut) => {
+                return (
+                  <Option key={aut.author_name} value={aut.author_name}>
+                    {aut.author_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create authors
+          </button>
+          <Select
+            mode="multiple"
+            onChange={handleChange}
+            name="categories"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+            placeholder="Select categories"
+          >
+            {allCategories &&
+              allCategories.map((cat) => {
+                return (
+                  <Option key={cat.category_name} value={cat.category_name}>
+                    {cat.category_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Categories
+          </button>
+          <Select
+            mode="multiple"
+            onChange={handleChange}
+            name="designers"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+            placeholder="Select designers"
+          >
+            {allDesigners &&
+              allDesigners.map((des) => {
+                return (
+                  <Option key={des.designer_name} value={des.designer_name}>
+                    {des.designer_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Designers
+          </button>
+          <Select
+            mode="multiple"
+            name="editorial"
+            onChange={handleChange}
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+            placeholder="Select editorials"
+          >
+            {allEditorials &&
+              allEditorials.map((edit) => {
+                return (
+                  <Option key={edit.editorial_name} value={edit.editorial_name}>
+                    {edit.editorial_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Editorials
+          </button>
+          <Select
+            onChange={handleChange}
+            name="mechanic"
+            mode="multiple"
+            placeholder="Select mechanics"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+          >
+            {allMechanics &&
+              allMechanics.map((mec) => {
+                return (
+                  <Option key={mec.mechanic_name} value={mec.mechanic_name}>
+                    {mec.mechanic_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Mechanics
+          </button>
+          <Select
+            onChange={handleChange}
+            name="thematic"
+            mode="multiple"
+            placeholder="Select thematics"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+            }}
+          >
+            {allThematics &&
+              allThematics.map((them) => {
+                return (
+                  <Option key={them.thematic_name} value={them.thematic_name}>
+                    {them.thematic_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Thematics
+          </button>
+          <Select
+            onChange={handleChange}
+            name="thematic"
+            mode="multiple"
+            placeholder="Select thematics"
+            style={{
+              width: "30%",
+              margin: "0.5rem",
+              boxShadow: " 0 2px 4px rgba(0, 0, 0, 0.4)",
+              fontSize: "medium",
+              borderRadius: "8px",
+            }}
+          >
+            {allLanguages &&
+              allLanguages.map((len) => {
+                return (
+                  <Option key={len.language_name} value={len.language_name}>
+                    {len.language_name}
+                  </Option>
+                );
+              })}
+          </Select>
+          <button className="create-button" onClick={handleModalCreate}>
+            Create Languages
+          </button>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+        <CreateCategories setIsOpen={handleModalCreate} isOpen={modal} />
+      </div>
+    </>
   );
 }
---
+++ ecommerce_front/src/components/Admin/Forms/validations.js
@@ -0,0 +1,63 @@
+const validations = (input) => {
+  let regexGameName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexReleased = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+  let regexNumberDecimal = /^\d+(\.\d+)?$/;
+  let regexPlusNumber = /^\+([0-9]{2})$/;
+  let regexNumber = /^-?\d+$/;
+  const errors = {};
+
+  if (!input.name) {
+    errors.name = "Name is required";
+  } else if (!regexGameName.test(input.name)) {
+    errors.name = "Game name only accept letters and blank spaces";
+  }
+
+  if (!input.released) {
+    errors.released = "Date is required";
+  } else if (!regexReleased.test(input.released)) {
+    errors.released = "Date must be in date format";
+  }
+
+  if (!input.price) {
+    errors.price = "Price is required";
+  } else if (!regexNumberDecimal.test(input.price)) {
+    errors.price = "Price must be a number";
+  }
+
+  if (!input.weight) {
+    errors.weight = "Weight is required";
+  } else if (!regexNumberDecimal.test(input.weight)) {
+    errors.weight = "Weight must be a number";
+  }
+  if (!input.age) {
+    errors.age = "Age is required";
+  } else if (!regexPlusNumber.test(input.age)) {
+    errors.age = "Age must be written like this: +15";
+  }
+  if (!input.players_min) {
+    errors.players_min = "Players Min is required";
+  } else if (!regexNumber.test(input.players_min)) {
+    errors.players_min = "Players Min must be a number";
+  }
+  if (!input.players_max) {
+    errors.players_max = "Players Max is required";
+  } else if (!regexNumber.test(input.players_max)) {
+    errors.players_max = "Players Min must be a number";
+  } else if (input.players_max < input.players_min) {
+    errors.players_max = " Players Max must be a larger than Players Min";
+  }
+  if (!input.playing_time) {
+    errors.playing_time = "Playing Time is required";
+  } else if (!regexNumber.test(input.playing_time)) {
+    errors.playing_time = "Players time  must be a number";
+  }
+  if (!input.stock) {
+    errors.stock = "Stock is required";
+  } else if (!regexNumber.test(input.stock)) {
+    errors.stock = "Stock must be a number";
+  }
+
+  return errors;
+};
+
+export default validations;
