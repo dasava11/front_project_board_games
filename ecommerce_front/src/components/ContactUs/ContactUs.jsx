b66fe926a70b728559382c46dd97b91aa627e5e3
Mek <99274729+mgs1987@users.noreply.github.com>
2023-06-26 13:50:53 -0400

    contactus css

--- ecommerce_front/src/components/ContactUs/ContactUs.jsx
+++ ecommerce_front/src/components/ContactUs/ContactUs.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './ContactUs.css';
+import "./ContactUs.css";
 
 const ContactUs = () => {
   return (
@@ -8,17 +7,36 @@ const ContactUs = () => {
       <form>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" placeholder="Enter your name" />
+          <input
+            className="input-contactus"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" placeholder="Enter your email" />
+          <input
+            className="input-contactus"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" rows="4" placeholder="Enter your message" />
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            placeholder="Enter your message"
+          />
         </div>
-        <button type="submit" className="submit-btn">Submit</button>
+        <button type="submit" className="submit-btn">
+          Submit
+        </button>
       </form>
       <div className="contact-info">
         <h3>Contact Information</h3>
