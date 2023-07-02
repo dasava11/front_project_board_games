9c9f79706e05a31bc33a95927931a6b25462771d
unknown <ddandolo@yahoo.com>
2023-07-02 18:00:47 -0300

    validaciones de carrito

--- ecommerce_front/src/components/Cart/Cart.jsx
+++ ecommerce_front/src/components/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import styles from "./Cart.module.css";
 
@@ -42,12 +42,10 @@ const Cart = () => {
               key={order.indexOf(game)}
             >
               <div className={styles.imgInCart}>
-              <img src={game.image?.url}
-                alt={game.name}
-              />
+                <img src={game.image?.url} alt={game.name} />
               </div>
               <div>
-              <h1 className={styles.nameGameInOrder}>{game.name}</h1>
+                <h1 className={styles.nameGameInOrder}>{game.name}</h1>
               </div>
               <div>
                 <button
@@ -59,6 +57,7 @@ const Cart = () => {
                   -
                 </button>
                 <input
+                  disabled
                   className={styles.countGameInOrder}
                   type="text"
                   placeholder={game.count}
@@ -66,12 +65,13 @@ const Cart = () => {
                 <button
                   className={styles.gameAmountBtn}
                   value={`${game.game_id}_increase`}
+                  disabled={game.count >= game.stock ? true : false}
                   onClick={handleAmount}
                 >
                   +
                 </button>
-                </div>
-                <div>
+              </div>
+              <div>
                 <button
                   value={game.game_id}
                   className={styles.gameDeleteByOrder}
@@ -81,23 +81,28 @@ const Cart = () => {
                 </button>
               </div>
               <div>
-              <h1 className={styles.priceOrder}>
-                $ {(game.price * game.count).toFixed(2)} USD
-              </h1>
+                <h1 className={styles.priceOrder}>
+                  $ {(game.price * game.count).toFixed(2)} USD
+                </h1>
               </div>
             </div>
           );
         })}
-      <div className= {styles.checkoutContainer}>
-        <div className = {styles.totalContainer}>
-        <h1 className={styles.total}>TOTAL</h1>
-        {order &&
-          order.map((game) => {
-            suma = suma + Number(game.price * game.count);
-          })}
-        <h1 className={styles.totalPriceOrder}>$ {suma.toFixed(2)} USD</h1>
+      <div className={styles.checkoutContainer}>
+        <div className={styles.totalContainer}>
+          <h1 className={styles.total}>TOTAL</h1>
+          {order &&
+            order.map((game) => {
+              suma = suma + Number(game.price * game.count);
+            })}
+          <h1 className={styles.totalPriceOrder}>$ {suma.toFixed(2)} USD</h1>
         </div>
-        <button className = {styles.gameDeleteByOrder} onClick = {() => navigate("/paypal")}>CHECK OUT</button>
+        <button
+          className={styles.gameDeleteByOrder}
+          onClick={() => navigate("/paypal")}
+        >
+          CHECK OUT
+        </button>
       </div>
     </div>
   );
