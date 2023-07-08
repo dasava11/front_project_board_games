9e11f4437ea40cb7a76ad839446acf57de7c929d
Erik Mortensen <mortensenerikm@gmail.com>
2023-07-08 18:16:31 -0300

    SignUp 9 puntos

--- ecommerce_front/src/components/SignUp/SignUp.jsx
+++ ecommerce_front/src/components/SignUp/SignUp.jsx
@@ -4,20 +4,39 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../Auth/authContext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { passwordDifficulty, validateForm, validateInput } from "./validate";
 
 export const SignUp = () => {
   const navigate = useNavigate();
 
-  const [user, setUser] = useState({
+  const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
+  const [errors, setErrors] =  useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const validForm = validateForm(form,errors)
+  const passDifficulty = passwordDifficulty(form.password);
+  // let passDifficulty = '';
+
   const { signup, controlarEmail } = useAuth();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
+    setForm({ ...form, [name]: value });
+    validateInput({
+      ...form, [name]: value },
+      errors,
+      setErrors);
+      // if(name === "password"){
+      //   passDifficulty = passwordDifficulty(value);
+      //   console.log(passDifficulty)
+      // }
   };
 
   const handleSubmit = async (e) => {
@@ -40,10 +59,14 @@ export const SignUp = () => {
 
     }
   };
+
+
   return (
     <div className="signup-container">
-      <form className="signup-form" onSubmit={(e) => handleSubmit(e)}>
+      <form className="signup-form" onSubmit={handleSubmit}>
         <h2 className="create-account">Create your account</h2>
+
+<div className="div-inputs">
 
         <label htmlFor="name">Full Name:</label>
         <input
@@ -51,19 +74,20 @@ export const SignUp = () => {
           type="text"
           name="name"
           id="name"
-          value={user.name}
+          value={form.name}
           onChange={handleChange}
-        ></input>
+          />
+        <span className="inputs-errors">{errors.name}</span>
         <label htmlFor="email">Email:</label>
         <input
           className="inputs-signup"
           type="text"
           name="email"
           id="email"
-          value={user.email}
-          placeholder="youremail@yourcompany.com"
+          value={form.email}
           onChange={handleChange}
-        ></input>
+          />
+        <span className="inputs-errors">{errors.email}</span>
 
         <label htmlFor="password">Password:</label>
         <input
@@ -71,11 +95,14 @@ export const SignUp = () => {
           type="password"
           name="password"
           id="password"
-          value={user.password}
+          value={form.password}
           onChange={handleChange}
-        ></input>
+          />
+        <span className="inputs-errors">{errors.password}</span>
+        <p className={`signup-password-${passDifficulty}`}>{passDifficulty}</p>
+</div>
 
-        <button type="submit" className="signup-button">
+        <button type="submit" className="signup-button" disabled={!validForm}>
           Create Account
         </button>
         <span>
