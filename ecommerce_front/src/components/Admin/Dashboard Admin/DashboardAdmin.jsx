dbcf811df8e238bede59e0ed4366aedc4811ae67
David Sánchez Vargas <david@192.168.0.12>
2023-07-09 12:50:27 -0500

    try create game

--- ecommerce_front/src/components/Admin/Dashboard Admin/DashboardAdmin.jsx
+++ ecommerce_front/src/components/Admin/Dashboard Admin/DashboardAdmin.jsx
@@ -5,6 +5,10 @@ import { getAllGames } from "../../../Redux/actions_creators";
 import { Link } from "react-router-dom";
 import styles from "./test.module.css";
 import { HeaderAdmin } from "../HeaderAdmin/HeaderAdmin";
+import { LikeOutlined } from "@ant-design/icons";
+import { AntDesignOutlined } from "@ant-design/icons";
+import { Col, Row, Statistic, Avatar } from "antd";
+
 export const DashboardAdmin = () => {
   const dispatch = useDispatch();
   const games = useSelector((state) => state.allGames);
@@ -16,9 +20,24 @@ export const DashboardAdmin = () => {
   return (
     <div className={styles.containerAdmin}>
       <HeaderAdmin />
-      <br />
-      <br />
-      <br />
+      <div className={styles.dashBoardAd}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Statistic
+              title="Feedback"
+              value={1128}
+              prefix={<LikeOutlined />}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Unmerged" value={93} suffix="/ 100" />
+          </Col>
+        </Row>
+        <Avatar
+          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+          icon={<AntDesignOutlined />}
+        />
+      </div>
     </div>
   );
 };
