c323bd4877476f88bd83d72fe73ec87b0d4d6b39
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-07-13 19:28:28 -0500

    prettier

--- ecommerce_front/src/components/User/User.jsx
+++ ecommerce_front/src/components/User/User.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import UserMenu from './UserMenu/UserMenu'
-import userSvg from '../../Photos/userSvg.svg'
-import style from './User.module.css'
-
+import React from "react";
+import UserMenu from "./UserMenu/UserMenu";
+import userSvg from "../../Photos/userSvg.svg";
+import style from "./User.module.css";
 
 const User = () => {
   return (
     <div className={style.userContainer}>
-        <div className={style.userImg}>
-            <img src={userSvg} alt="user logo" />
-            <div>
-                <h1>User´s name</h1>
-                <h2>email@email.com</h2>
-            </div>
+      <div className={style.userImg}>
+        <img src={userSvg} alt="user logo" />
+        <div>
+          <h1>User´s name</h1>
+          <h2>email@email.com</h2>
         </div>
-        <UserMenu/>
+      </div>
+      <UserMenu />
     </div>
-    
-  )
-}
+  );
+};
 
-export default User
+export default User;
