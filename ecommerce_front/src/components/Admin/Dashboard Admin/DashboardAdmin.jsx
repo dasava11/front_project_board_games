a99b7db005c6060585ced51740eb3cbd1b7ea785
David Sánchez Vargas <david@192.168.0.12>
2023-07-16 22:50:05 -0500

    renderizando visual de user

--- ecommerce_front/src/components/Admin/Dashboard Admin/DashboardAdmin.jsx
+++ ecommerce_front/src/components/Admin/Dashboard Admin/DashboardAdmin.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllGames } from "../../../Redux/actions_creators";
 import { Link } from "react-router-dom";
 import styles from "./test.module.css";
-
 import { LikeOutlined } from "@ant-design/icons";
 import { AntDesignOutlined } from "@ant-design/icons";
 import { Col, Row, Statistic, Avatar } from "antd";
@@ -49,22 +48,6 @@ export const DashboardAdmin = () => {
       </div>
       <div>
         <br />
-        <Row gutter={16}>
-          <Col span={12}>
-            <Statistic
-              title="Feedback"
-              value={1128}
-              prefix={<LikeOutlined />}
-            />
-          </Col>
-          <Col span={12}>
-            <Statistic title="Unmerged" value={93} suffix="/ 100" />
-          </Col>
-        </Row>
-        <Avatar
-          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-          icon={<AntDesignOutlined />}
-        />
       </div>
     </div>
   );
