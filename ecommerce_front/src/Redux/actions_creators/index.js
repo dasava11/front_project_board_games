b530fbdcfd8c4ad0032933a6ed8448ba42f1efe3
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-07-19 12:54:27 -0600

    arreglando errores

--- ecommerce_front/src/Redux/actions_creators/index.js
+++ ecommerce_front/src/Redux/actions_creators/index.js
@@ -15,8 +15,7 @@ import {
   GET_ALL_USERS,
   GET_ALL_PURCHASES,
   SET_DARK_MODE,
-  GET_USER_BY_ID
-
+  GET_USER_BY_ID,
 } from "../action-types/index";
 import { toast } from "react-toastify";
 
@@ -261,24 +260,25 @@ export const getAllUsers = () => {
       console.error(error);
     }
   };
+};
 
-export const getAllPurchases =()=>{
- return async (dispatch)=>{
-  try {
-    const resp=await axios.get(VITE_URL_GET_PURCHASES)
-    dispatch({ type: GET_ALL_PURCHASES, payload: response.data})
-  } catch(err){
-    console.error(err)
-  }
- }
-}
-export const getUserById=(id)=>{
-  return async (dispatch)=>{
- try {
-  const resp = await axios.get(`${VITE_URL_USERS}/${id}`)
-  dispatch({type: GET_USER_BY_ID, payload: resp.data})
- } catch (error) {
-  console.error(error)
- }
-  }
- }
+export const getAllPurchases = () => {
+  return async (dispatch) => {
+    try {
+      const resp = await axios.get(VITE_URL_GET_PURCHASES);
+      dispatch({ type: GET_ALL_PURCHASES, payload: response.data });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+export const getUserById = (id) => {
+  return async (dispatch) => {
+    try {
+      const resp = await axios.get(`${VITE_URL_USERS}/${id}`);
+      dispatch({ type: GET_USER_BY_ID, payload: resp.data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
