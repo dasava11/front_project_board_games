49c18db4ed5b1e709fb1d9d17db0ca752c1357f0
nahuel <lescanoagustin10nahuel@gmail.com>
2023-07-19 20:25:59 -0300

    Minor changes.

--- ecommerce_front/src/components/Paypal/Paypal.jsx
+++ ecommerce_front/src/components/Paypal/Paypal.jsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { PayPalButtons } from '@paypal/react-paypal-js';
-import styles from './Paypal.module.css';
+import React, { useState } from "react";
+import { useAuth } from "../Auth/authContext";
+import { useLocation, useNavigate } from "react-router-dom";
+import { PayPalButtons } from "@paypal/react-paypal-js";
+import styles from "./Paypal.module.css";
+import axios from "axios";
+
+const VITE_URL_PAYPAL = import.meta.env.VITE_URL_PAYPAL;
 
 const PayPalPaymentButton = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { userAuth } = useAuth();
   const { amount, buys } = location.state;
   const [showSummary, setShowSummary] = useState(false);
-  const [orderId, setOrderId] = useState('');
+  const [orderId, setOrderId] = useState("");
 
-  const onSuccess = (data) => {
-    const newData = { ...data, buys, amount };
-    console.log('Pago exitoso:', newData);
-    setOrderId(newData.orderID);
-    setShowSummary(true);
+  const onSuccess = async () => {
+    const newData = {
+      games: buys.map((item) => ({
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+        user_id: userAuth.uid,
+      })),
+      total_amount: amount,
+    };
+
+    console.log("Pago exitoso:", newData);
+
+    try {
+      await axios.post(VITE_URL_PAYPAL, newData);
+      setShowSummary(true);
+    } catch (error) {
+      alert(error.message);
+    }
   };
 
   const handleCloseSummary = () => {
     setShowSummary(false);
-    navigate('/cart'); 
+    navigate("/games");
   };
 
   return (
@@ -45,9 +64,8 @@ const PayPalPaymentButton = () => {
       {showSummary && (
         <div className={styles.summaryModal}>
           <div className={styles.summaryContent}>
-
             <h2 className={styles.h1Purchase}>Resumen de la compra</h2>
-            <h3 className={styles.orderID}>Order Id: {orderId}</h3> 
+            <h3 className={styles.orderID}>Order Id: {orderId}</h3>
             <ul>
               {buys.map((item, index) => (
                 <li key={index}>
