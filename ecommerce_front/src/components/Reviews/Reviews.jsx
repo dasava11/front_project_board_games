899f5e3c017ab21ed1f6faed8be5522911203ae0
Ricardo Gomez Aguirre <ricardog@Ricardos-MacBook-Air.local>
2023-07-22 00:11:22 -0600

    cambio link

--- ecommerce_front/src/components/Reviews/Reviews.jsx
+++ ecommerce_front/src/components/Reviews/Reviews.jsx
@@ -7,13 +7,16 @@ import axios from "axios";
 const VITE_URL_REVIEWS = import.meta.env.VITE_URL_REVIEWS;
 
 const Reviews = () => {
+  const { id } = useParams();
   const [reviews, setReviews] = useState();
+
+  console.log(id);
 
   useEffect(() => {
     const fetchReviews = async () => {
       try {
         const response = await axios.get(
-          "https://backprojectboardgames-production.up.railway.app/reviews"
+          `https://backprojectboardgames-production.up.railway.app/reviews/idGame/${id}`
         );
         setReviews(response.data);
       } catch (error) {
